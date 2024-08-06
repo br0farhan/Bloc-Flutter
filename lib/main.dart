9c45fa4c4e0b5575bb2c1adddfa36dbbcf228b59
Farhan Martiyansyah <far.martiyansyah@gmail.com>
2024-08-06 16:14:15 +0700

    update multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:bloc_flutter/app.dart';
 import 'package:bloc_flutter/bloc/counter.dart';
-
-import 'package:bloc_flutter/routes/routes.dart';
+import 'package:bloc_flutter/bloc/theme.dart';
+import 'package:bloc_flutter/pages/home.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  final router = appRouter();
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: router.onRoute ,
+   return MultiBlocProvider(providers: [
+    BlocProvider(create: 
+    (context) => CounterBloc(),
+    ),
+     BlocProvider(create: 
+    (context) => ThemeBloc(),
+    ),
+   ], child: App()
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,84 +1,55 @@
 import 'package:bloc_flutter/bloc/counter.dart';
-import 'package:bloc_flutter/pages/data_widget.dart';
+import 'package:bloc_flutter/bloc/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-// ignore: must_be_immutable
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePages extends StatelessWidget {
+  const HomePages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Counter myCounter = context.read<Counter>();
+    CounterBloc myCounter = context.read<CounterBloc>();
+    ThemeBloc myTheme = context.read<ThemeBloc>();
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          child: const Icon(
-            Icons.forward,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            // Navigator.of(context).push(MaterialPageRoute(
-            //   builder: (context) {
-            //     return BlocProvider.value(
-            //       value: myCounter,
-            //       child: const OtherPages(),
-            //     );
-            //   },
-            // ));
-            Navigator.pushNamed(context, "otherPages");
-          }),
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 0, 104, 189),
-        title: const Text(
-          "Bloc Access",
-          style: TextStyle(color: Colors.white),
-        ),
+        backgroundColor: Colors.orange,
+        title: const Text("Multi Bloc Provider"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Material(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(15),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(15),
-                  onTap: () {
-                    myCounter.decrement();
-                  },
-                  child: const SizedBox(
-                    height: 100,
-                    width: 70,
-                    child: Center(
-                      child: Icon(Icons.remove),
-                    ),
-                  ),
-                ),
-              ),
-              const DataWidget(),
-              Material(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(15),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(15),
-                  onTap: () {
-                    myCounter.increment();
-                  },
-                  child: const SizedBox(
-                    height: 100,
-                    width: 70,
-                    child: Center(
-                      child: Icon(Icons.add),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.switch_access_shortcut),
+          onPressed: () {
+            myTheme.changeTheme();
+          }),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterBloc, int>(
+              bloc: myCounter,
+              builder: (context, state) {
+                return Text(
+                  "$state",
+                  style: TextStyle(fontSize: 50),
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      myCounter.remove();
+                    },
+                    icon: const Icon(Icons.remove)),
+                IconButton(
+                    onPressed: () {
+                      myCounter.add();
+                    },
+                    icon: const Icon(Icons.add))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }

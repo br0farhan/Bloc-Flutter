3ae13d46c515f59df2b24036cce66808ab45e994
Farhan Martiyansyah <far.martiyansyah@gmail.com>
2024-08-07 11:16:46 +0700

    update bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:bloc_flutter/app.dart';
-import 'package:bloc_flutter/bloc/counter.dart';
-import 'package:bloc_flutter/bloc/theme.dart';
+import 'package:bloc_flutter/bloc/user.dart';
 import 'package:bloc_flutter/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,14 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return MultiBlocProvider(providers: [
-    BlocProvider(create: 
-    (context) => CounterBloc(),
-    ),
-     BlocProvider(create: 
-    (context) => ThemeBloc(),
-    ),
-   ], child: App()
+    return MaterialApp(
+      home: BlocProvider(
+        create: (context) => UserBloc(),
+        child: const  HomePage(),
+      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,91 +1,83 @@
-import 'package:bloc_flutter/bloc/counter.dart';
-import 'package:bloc_flutter/bloc/theme.dart';
+import 'package:bloc_flutter/bloc/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePages extends StatelessWidget {
-  const HomePages({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    CounterBloc myCounter = context.read<CounterBloc>();
-    ThemeBloc myTheme = context.read<ThemeBloc>();
+    UserBloc userBloc = context.read<UserBloc>();
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: const Text("Multi Bloc Listener"),
+        backgroundColor: Colors.blueGrey,
+        title: Text("Bloc Selector"),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.switch_access_shortcut),
-          onPressed: () {
-            myTheme.changeTheme();
-          }),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
           children: [
-            MultiBlocListener(
-              listeners: [
-                BlocListener<CounterBloc, int>(
-                  listenWhen: (previous, current) {
-                    if (current > 10) {
-                      return true;
-                    }
-                    return false;
-                  },
-                  listener: (context, state) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Datanya lebih dari 10"),
-                      ),
-                    );
-                  },
-                ),
-                BlocListener<ThemeBloc, bool>(
-                  listenWhen: (previous, current) {
-                    if (current == false) {
-                      return true;
-                    }
-                    return false;
-                  },
-                  listener: (context, state) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        duration: Duration(seconds: 1),
-                        content: Text("Theme is Dark"),
-                      ),
-                    );
-                  },
-                )
-              ],
-              child: BlocBuilder<CounterBloc, int>(
-                bloc: myCounter,
-                builder: (context, state) {
-                  return Text(
-                    "$state",
-                    style: const TextStyle(fontSize: 50),
-                  );
-                },
-              ),
+            BlocSelector<UserBloc, Map<String, dynamic>, String>(
+              selector: (state) => state["name"],
+              builder: (context, state) {
+                print("Text build : Nama");
+                return Text("NAMA :$state");
+              },
+            ),
+            BlocSelector<UserBloc, Map<String, dynamic>, int>(
+              selector: (state) => state["age"],
+              builder: (context, state) {
+                print("Text build : Umur");
+                return Text("Umur :$state");
+              },
+            ),
+            // TODO : Jika menggunakan Bloc Builder selama dalam wdiget didalamnya akan terbuild ulang semua
+            // BlocBuilder<UserBloc, Map<String, dynamic>>(
+            //   bloc: userBloc,
+            //   builder: (context, state) {
+            //     print("Text build : Nama");
+            //     return Text("NAMA :${state["name"]}");
+            //   },
+            // ),
+            // BlocBuilder<UserBloc, Map<String, dynamic>>(
+            //   bloc: userBloc,
+            //   builder: (context, state) {
+            //       print("Text build : Umur");
+            //     return Text("UMUR :${state["age"]} Tahun");
+            //   },
+            // ),
+
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) {
+                userBloc.changeName(value);
+              },
+              decoration: InputDecoration(border: OutlineInputBorder()),
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 IconButton(
                     onPressed: () {
-                      myCounter.remove();
+                      userBloc.changeAge(userBloc.state["age"] - 1);
                     },
                     icon: const Icon(Icons.remove)),
                 IconButton(
                     onPressed: () {
-                      myCounter.add();
+                      userBloc.changeAge(userBloc.state["age"] + 1);
                     },
                     icon: const Icon(Icons.add))
               ],
             )
           ],
         ),
-      ),
+      )),
     );
   }
 }
